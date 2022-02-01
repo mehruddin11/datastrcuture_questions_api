09fc5d21ceeaa028fab430e3ee9a43d7fe7f4254
mehruddin11 <mehruddin881@gmail.com>
2022-02-01 17:40:19 +0530

    adding changes

--- data.js
+++ data.js
@@ -5,22 +5,22 @@ const Question = () =>[
         type:'Easy',
         question:'What is a Data Structure?',
         solution:'solution',
-        data:new Date().getDate(),
+        date:new Date().getDate(),
             
     },
     {
         id :'2',
         type:'hard',
-        question:'Describe the types of Data Structures?',
+        question:'Describe the types of data Structures?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
         id :'3',
         type:'hard',
-        question:' What is a Linear Data Structure? Name a few examples?',
+        question:' What is a Linear data Structure? Name a few examples?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
          
@@ -28,7 +28,7 @@ const Question = () =>[
         type:'Easy',
         question:'What is the difference between file structure and storage structure?',
         solution:'solution',
-        data:new Date().getDate(),
+        date:new Date().getDate(),
             
     },
     {
@@ -36,78 +36,78 @@ const Question = () =>[
         type:'hard',
         question:'How are the elements of a 2D array stored in the memory?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
         id :'6',
         type:'hard',
-        question:' What is a linked list Data Structure?',
+        question:' What is a linked list data Structure?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     
     {
             id :'7',
             type:'hard',
-            question:'Are linked lists considered linear or non-linear Data Structures?',
+            question:'Are linked lists considered linear or non-linear data Structures?',
             solution:'solution',
-            data:new Date().getDate()
+            date:new Date().getDate()
     },
     {
         id :'8',
         type:'hard',
         question:' What are the advantages of a linked list over an array? In which scenarios do we use Linked List and when Array?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
         id :'9',
         type:'hard',
         question:' What is a doubly-linked list? Give some examples.',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
         id :'10',
         type:'hard',
         question:' How do you reference all of the elements in a one-dimension array?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
         id :'11',
         type:'hard',
-        question:' What are dynamic Data Structures? Name a few?',
+        question:' What are dynamic data Structures? Name a few?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
         id :'12',
         type:'hard',
         question:'What is an algorithm? ',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
         id :'13',
         type:'hard',
         question:' Why do we need to do an algorithm analysis?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
         id :'14',
         type:'hard',
         question:'What is a stack?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
         id :'15',
         type:'hard',
         question:'  Where are stacks used?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
          
@@ -115,7 +115,7 @@ const Question = () =>[
         type:'Easy',
         question:'What are the operations that can be performed on a stack?',
         solution:'solution',
-        data:new Date().getDate(),
+        date:new Date().getDate(),
             
     },
     {
@@ -123,22 +123,22 @@ const Question = () =>[
         type:'hard',
         question:'What is a postfix expression?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
         id :'18',
         type:'hard',
-        question:'  What is a queue Data Structure?',
+        question:'  What is a queue data Structure?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
          
         id :'19',
         type:'Easy',
-        question:'List some applications of queue Data Structure.',
+        question:'List some applications of queue data Structure.',
         solution:'solution',
-        data:new Date().getDate(),
+        date:new Date().getDate(),
             
     },
     {
@@ -146,14 +146,14 @@ const Question = () =>[
         type:'hard',
         question:'What is a Dequeue?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
         id :'21',
         type:'hard',
         question:'What operations can be performed on queues?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     
     {
@@ -161,63 +161,63 @@ const Question = () =>[
             type:'hard',
             question:'What are the advantages of the heap over a stack?',
             solution:'solution',
-            data:new Date().getDate()
+            date:new Date().getDate()
     },
     {
         id :'23',
         type:'hard',
-        question:' Where can stack Data Structure be used?',
+        question:' Where can stack data Structure be used?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
         id :'24',
         type:'hard',
         question:'What is the difference between a PUSH and a POP?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
         id :'25',
         type:'hard',
         question:' Which sorting algorithm is considered the fastest? Why?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
         id :'26',
         type:'hard',
         question:'What is the merge sort? How does it work?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
         id :'27',
         type:'hard',
         question:' How does the Selection sort work?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
         id :'28',
         type:'hard',
         question:'What is an asymptotic analysis of an algorithm?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
         id :'29',
         type:'hard',
         question:'What are asymptotic notations?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     {
         id :'30',
         type:'hard',
         question:'What are some examples of divide and conquer algorithms?',
         solution:'solution',
-        data:new Date().getDate()
+        date:new Date().getDate()
     },
     
     
